3ce3e8a5e6ea60f3fd23c1e2889cf0755891585a
Emmanuel-Develops <itakpeemma@gmail.com>
2023-12-30 22:13:58 +0100

    feat: bump max spendable

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,4 +1,4 @@
 export const LN_ADDRESS_REGEX = /^[a-z0-9\-_\.]+$/i;
 
-export const MAX_SPENDABLE = 100000000 // 100000 sats
+export const MAX_SPENDABLE = 1000000000 // 1000000 sats
 export const MIN_SPENDABLE = 200000 // 200 sats
