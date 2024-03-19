c5286f8e3622b803c7a57d23e89e79ff78848f6f
Theophilus <isahtheophilus@gmail.com>
2024-03-19 04:04:18 +0100

    Fix URL formatting in getBaseUrl function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,6 @@ export const getBaseUrl = () => {
     return process.env.VERCEL_ENV === "production"
         ? API_DOMAIN
         : process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-          ? `https://www.${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL}`
+          ? `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL}`
           : "http://localhost:3000"
 }
